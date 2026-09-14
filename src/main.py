f5f4807e5d4190e43122732a296d4af0ba6a2305
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2026-09-14 20:16:15 +0530

    static site basics

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode,TextType
+
+def main():
+    dummy_text = TextNode("This is some anchor text",TextType.LINK,'https://www.boot.dev')
+    print(dummy_text)
+
+
+if __name__ == "__main__":
+    main()
